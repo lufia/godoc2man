820fa623b3bd2ab0c63426dfc6709c9ef559163f
lufia <lufia@lufia.org>
2025-09-21 18:50:02 +0900

    generate types, methods, more

--- print.go
+++ print.go
@@ -7,6 +7,7 @@ import (
 	"go/format"
 	"go/token"
 	"io"
+	"iter"
 	"path"
 	"strings"
 
@@ -31,7 +32,7 @@ func (p *Printer) Err() error {
 
 func (p *Printer) Command(pkg *doc.Package, d *comment.Doc, flags []*Flag) {
 	p.writeHeader(pkg, flags)
-	p.writeContent(d.Content, 0)
+	p.writeContent(d.Content, 0, false)
 	p.writeBugs(pkg.Notes["BUG"])
 }
 
@@ -62,7 +63,7 @@ func (p *Printer) writeHeader(pkg *doc.Package, flags []*Flag) {
 	fmt.Fprintln(p, ".SH OVERVIEW")
 }
 
-func (p *Printer) writeContent(content []comment.Block, depth int) {
+func (p *Printer) writeContent(content []comment.Block, depth int, cont bool) {
 	for _, c := range content {
 		switch c := c.(type) {
 		case *comment.Heading:
@@ -70,7 +71,7 @@ func (p *Printer) writeContent(content []comment.Block, depth int) {
 			fmt.Fprintf(w, ".SH %s", Text(c.Text))
 			fmt.Fprintln(p, "")
 		case *comment.Paragraph:
-			if depth == 0 {
+			if depth == 0 && !cont {
 				fmt.Fprintln(p, ".PP")
 			}
 			fmt.Fprintf(p, "%+s", Text(c.Text))
@@ -88,9 +89,10 @@ func (p *Printer) writeContent(content []comment.Block, depth int) {
 					symbol = item.Number + "."
 				}
 				fmt.Fprintf(p, ".IP %s 4\n", symbol)
-				p.writeContent(item.Content, depth+1)
+				p.writeContent(item.Content, depth+1, false)
 			}
 		}
+		cont = false
 	}
 }
 
@@ -176,13 +178,49 @@ func (p *Printer) Library(pkg *doc.Package, d *comment.Doc) {
 	fmt.Fprintln(p, ".nf")
 	fmt.Fprintf(p, ".B \"import \\(dq%s\\(dq\"\n", p.pkgPath)
 	fmt.Fprintln(p, ".sp")
-	for _, t := range pkg.Funcs {
-		fmt.Fprint(p, ".BI \"")
-		writeFunc(p, p.fset, t)
-		fmt.Fprintln(p, "\"")
+	for _, v := range pkg.Vars {
+		writeVar(p, p.fset, v)
+	}
+	ndef := len(pkg.Vars)
+	if ndef > 0 && len(pkg.Types) > 0 {
+		fmt.Fprint(p, "\n")
+		ndef = 0
+	}
+	ndef += len(pkg.Types)
+	for _, t := range pkg.Types {
+		writeType(p, p.fset, t)
+	}
+	if ndef > 0 && len(pkg.Funcs) > 0 {
+		fmt.Fprint(p, "\n")
+		ndef = 0
+	}
+	ndef += len(pkg.Funcs)
+	for _, f := range pkg.Funcs {
+		fmt.Fprint(p, `.BI "`)
+		writeFunc(p, p.fset, f)
+		fmt.Fprintln(p, `\"`)
 	}
 	fmt.Fprintln(p, ".fi")
 	fmt.Fprintln(p, ".SH DESCRIPTION")
+	p.writeContent(d.Content, 0, false)
+	if len(pkg.Vars) > 0 {
+		fmt.Fprintln(p, ".PP")
+	}
+	fmt.Fprintln(p, ".SS Variables")
+	for _, t := range pkg.Vars {
+		p.writeSymbolDoc(t.Doc, t.Names[0])
+	}
+	if len(pkg.Types) > 0 {
+		fmt.Fprintln(p, ".PP")
+	}
+	fmt.Fprintln(p, ".SS Types")
+	for _, t := range pkg.Types {
+		p.writeSymbolDoc(t.Doc, t.Name)
+	}
+	if len(pkg.Funcs) > 0 {
+		fmt.Fprintln(p, ".PP")
+	}
+	fmt.Fprintln(p, ".SS Functions")
 	for _, t := range pkg.Funcs {
 		s := t.Doc
 		if strings.HasPrefix(s, t.Name) {
@@ -191,13 +229,79 @@ func (p *Printer) Library(pkg *doc.Package, d *comment.Doc) {
 		}
 		var parser comment.Parser
 		doc := parser.Parse(strings.TrimSpace(s))
-		p.writeContent(doc.Content, 1)
+		p.writeContent(doc.Content, 0, true)
 		fmt.Fprintln(p, ".PP")
 	}
+	p.writeBugs(pkg.Notes["BUG"])
+}
+
+func writeType(w io.Writer, fset *token.FileSet, t *doc.Type) error {
+	if err := format.Node(w, fset, t.Decl); err != nil {
+		return err
+	}
+	_, err := fmt.Fprint(w, "\n")
+	for f := range mergeSlice(t.Funcs, t.Methods) {
+		fmt.Fprint(w, `.BI "`)
+		writeFunc(w, fset, f)
+		fmt.Fprintln(w, `\"`)
+	}
+	_, err = fmt.Fprint(w, "\n")
+	return err
+}
+
+func mergeSlice[S ~[]E, E any](s ...S) iter.Seq[E] {
+	return func(yield func(E) bool) {
+		for _, a := range s {
+			for _, v := range a {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func writeVar(w io.Writer, fset *token.FileSet, v *doc.Value) error {
+	if err := format.Node(w, fset, v.Decl); err != nil {
+		return err
+	}
+	_, err := fmt.Fprint(w, "\n")
+	return err
 }
 
 func writeFunc(w io.Writer, fset *token.FileSet, f *doc.Func) error {
 	x := *f.Decl
 	x.Body = nil
 	return format.Node(w, fset, &x)
+}
+
+func (p *Printer) writeSymbolDoc(s, name string) {
+	before, rest, ok := hasPrefix(s, name)
+	if ok {
+		fmt.Fprintln(p, before)
+		fmt.Fprintln(p, ".BR", name)
+		s = rest
+	}
+	var parser comment.Parser
+	doc := parser.Parse(strings.TrimSpace(s))
+	p.writeContent(doc.Content, 0, true)
+	fmt.Fprint(p, "\n")
+}
+
+func hasPrefix(s, name string) (before, rest string, ok bool) {
+	switch {
+	case strings.HasPrefix(s, "The "):
+		before = "The"
+		s = s[4:]
+	case strings.HasPrefix(s, "An "):
+		before = "An"
+		s = s[3:]
+	case strings.HasPrefix(s, "A "):
+		before = "A"
+		s = s[2:]
+	}
+	if strings.HasPrefix(s, name+" ") {
+		return before, s[len(name)+1:], true
+	}
+	return "", "", false
 }
